09639346d663fdb4d0bd2344fc27c3796a1f58b0
Abigail Swarth <abigailswarth@gmail.com>
2018-03-31 15:38:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = "bob"
+const leastFavoriteCustomer = 'time'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+ }
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite
+}
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'tim';
+}
